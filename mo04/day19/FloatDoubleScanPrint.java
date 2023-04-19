bfe978f85a8b19839dfbce05e06cd530e548d7ef
myeong <audelahd@gmail.com>
2023-04-19 17:35:05 +0900

    문자열 타입, 연산하기

---
+++ mo04/day19/FloatDoubleScanPrint.java
@@ -0,0 +1,21 @@
+package mo04.day19;
+
+import java.util.Scanner;
+
+public class FloatDoubleScanPrint {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("변수x는 float형입니다. 변수 y는 double형입니다.");
+		System.out.println("x : ");
+		float x = stdIn.nextFloat();
+		System.out.println("y : ");
+		double y = stdIn.nextDouble();
+		
+		System.out.println("x ="+x);
+		System.out.println("y= "+y);
+
+	}
+
+}
