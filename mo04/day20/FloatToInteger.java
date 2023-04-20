f9ed1e3d9e79c958f3d233694e20397dca78e9e0
myeong <audelahd@gmail.com>
2023-04-20 17:36:01 +0900

    배열만들기 및 배열 출력하기

---
+++ mo04/day20/FloatToInteger.java
@@ -0,0 +1,16 @@
+package mo04.day20;
+
+public class FloatToInteger {
+
+	public static void main(String[] args) {
+		int a;
+		
+		a= (int)10.0;
+		// a=10.0; //오류
+		
+		
+		System.out.println("a=" +a);
+
+	}
+
+}
